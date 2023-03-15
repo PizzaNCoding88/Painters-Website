cb7871d880dd8387c08719ff3955681ae19b615e
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2023-03-15 23:16:59 +0000

    tablet mode nearly done - footer missing

--- dist/app.js
+++ dist/app.js
@@ -7,22 +7,46 @@ burgerMenu.addEventListener("click" , () => {
 
 // Rectangles hover
 
-const smallRectangle = document.querySelector(".shape");
-const largeRectangle = document.querySelector(".rectangle");
-const bgImage = document.querySelector(".bg-image");
+const smallRectangle1 = document.querySelector(".shape-1");
+const smallRectangle2 = document.querySelector(".shape-2");
+const smallRectangle3 = document.querySelector(".shape-3");
+const smallRectangle4 = document.querySelector(".shape-4");
+const smallRectangle5 = document.querySelector(".shape-5");
+const smallRectangle6 = document.querySelector(".shape-6");
+// const largeRectangle = document.querySelector(".rectangle");
+const bgImage1 = document.querySelector(".bg-image1");
+const bgImage2 = document.querySelector(".bg-image2");
+const bgImage3 = document.querySelector(".bg-image3");
+const bgImage4 = document.querySelector(".bg-image4");
+const bgImage5 = document.querySelector(".bg-image5");
+const bgImage6 = document.querySelector(".bg-image6");
 const opacityText = document.querySelector(".opacity-text");
 
-function changeImage(){
-    bgImage.src="https://img.icons8.com/external-kmg-design-glyph-kmg-design/32/ffe468/external-paint-brush-graphic-design-kmg-design-glyph-kmg-design-1.png";
-    smallRectangle.style.backgroundColor = "#9dcf5e";
+function changeImage1(){
+    bgImage1.src="https://img.icons8.com/external-kmg-design-glyph-kmg-design/32/ffe468/external-paint-brush-graphic-design-kmg-design-glyph-kmg-design-1.png";
+    smallRectangle1.style.backgroundColor = "#9dcf5e";
     opacityText.style.opacity = "1";
 };
 
-function backToNormal(){
-    bgImage.src="https://img.icons8.com/external-kmg-design-glyph-kmg-design/32/9dcf5e/external-paint-brush-graphic-design-kmg-design-glyph-kmg-design-1.png";
-    smallRectangle.style.backgroundColor = "#ffe468";
+function backToNormal1(){
+    bgImage1.src="https://img.icons8.com/external-kmg-design-glyph-kmg-design/32/9dcf5e/external-paint-brush-graphic-design-kmg-design-glyph-kmg-design-1.png";
+    smallRectangle1.style.backgroundColor = "#ffe468";
     opacityText.style.opacity = "0";
 };
+
+function changeImage2(){
+    bgImage2.src="https://img.icons8.com/ios-glyphs/40/ffe468/roller-brush--v1.png";
+    smallRectangle2.style.backgroundColor = "#9dcf5e";
+    opacityText.style.opacity = "1";
+};
+
+function backToNormal2(){
+    bgImage2.src="https://img.icons8.com/ios-glyphs/40/9dcf5e/roller-brush--v1.png";
+    smallRectangle2.style.backgroundColor = "#ffe468";
+    opacityText.style.opacity = "0";
+};
+
+
 
 //testimonial scroll
 
@@ -35,5 +59,18 @@ nextBtn.addEventListener("click", () =>{
     if (testimonialOne.classList.contains("active")){
         testimonialOne.classList.remove("active");
         testimonialTwo.classList.add("active");
+    } else if (testimonialTwo.classList.contains("active")){
+        testimonialOne.classList.add("active");
+        testimonialTwo.classList.remove("active");
+    }
+});
+
+previousBtn.addEventListener("click", () =>{
+    if (testimonialOne.classList.contains("active")){
+        testimonialOne.classList.remove("active");
+        testimonialTwo.classList.add("active");
+    } else if (testimonialTwo.classList.contains("active")){
+        testimonialOne.classList.add("active");
+        testimonialTwo.classList.remove("active");
     }
 });
